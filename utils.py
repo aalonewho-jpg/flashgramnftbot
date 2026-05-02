37e00d68c3663cb0692de907a8a72a69e932ad4b
aalonewho-jpg <aalonewho@gmail.com>
2026-05-02 13:45:34 +0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -4,88 +4,130 @@ import asyncio
 from aiogram import Bot
 from database import add_stars, add_to_inventory
 
+# ========== НОВЫЕ ДАННЫЕ ДЛЯ БЕСПЛАТНОГО КЕЙСА ==========
 FREE_CASE_REWARDS = {
-    "1000 Stars": 10,
-    "500 Stars": 20,
-    "100 Stars": 50,
-    "Jelly Bunny": 5,
-    "Flying Broom": 4,
-    "Spiced Wine": 3,
-    "Lunar Snake": 3,
-    "Lol Pop": 1,
-    "Pet Snake": 1,
-    "Snake Box": 1,
-    "Ice Cream": 1,
-    "Witch Hat": 1
+    "25 Stars": 55,
+    "30 Stars": 25,
+    "50 Stars": 10,
+    "Lol Pop": 5,
+    "Candy Cane": 5
 }
 
+# ========== НОВЫЕ ДАННЫЕ ДЛЯ КЕЙСОВ (ЦЕНЫ И ШАНСЫ) ==========
 CASES_DATA = {
     "Фелириум": {
-        "500 Stars": 10, "250 Stars": 15, "100 Stars": 55,
-        "Sharp Tongue": 1, "Snoop Dogg": 9, "Candy Cane": 3,
-        "Lol Pop": 5, "Moon Pendant": 2
+        "price": 30,
+        "rewards": {
+            "75 Stars": 10, "50 Stars": 20, "25 Stars": 60,
+            "Lol Pop": 5, "Candy Cane": 5
+        }
     },
     "Запах": {
-        "800 Stars": 10, "500 Stars": 25, "250 Stars": 45,
-        "Jelly Bunny": 5, "Mad Pumpkin": 5, "Clover Pin": 5,
-        "Record Player": 4, "Perfume Bottle": 1
+        "price": 40,
+        "rewards": {
+            "75 Stars": 10, "50 Stars": 20, "25 Stars": 60,
+            "Jelly Bunny": 5, "Perfume Bottle": 5
+        }
     },
     "Богач": {
-        "1000 stars": 15, "800 stars": 20, "500 stars": 40,
-        "Hanging Star": 5, "Low Rider": 5, "Gem Signet": 1,
-        "Evil Eye": 9, "Voodoo Doll": 5
+        "price": 50,
+        "rewards": {
+            "90 Stars": 10, "60 Stars": 20, "40 Stars": 60,
+            "Evil Eye": 5, "Candy Cane": 5
+        }
     },
     "Свадьба": {
-        "2000Stars": 15, "1500 Stars": 50, "Flying Broom": 10,
-        "Spiced Wine": 5, "Jelly Bunny": 10, "Party Sparkler": 5,
-        "Xmas Stocking": 4, "Trapped Heart": 1
+        "price": 60,
+        "rewards": {
+            "100 Stars": 10, "80 Stars": 20, "55 Stars": 60,
+            "Spiced Wine": 5, "Xmas Stocking": 5
+        }
     },
     "Шахта": {
-        "3000 stars": 15, "2000 stars": 25, "1000 stars": 50,
-        "Happy Brownie": 1, "Jester Hat": 2, "Jingle Bells": 1,
-        "Stellar Rocket": 2, "Party Sparkler": 4
+        "price": 80,
+        "rewards": {
+            "110 Stars": 10, "95 Stars": 20, "60 Stars": 60,
+            "Happy Brownie": 5, "Jester Hat": 5
+        }
     },
     "Все или ничего": {
-        "8000 stars": 10, "6000 stars": 10, "3500 stars": 40,
-        "Perfume Bottle": 1, "Lunaer Snake": 4, "Snake Box": 5,
-        "Lol Pop": 3, "Whip Cupcake": 9, "Trapped Heart": 1,
-        "Spy Agaric": 7, "Ice Cream": 10
+        "price": 80,
+        "rewards": {
+            "150 Stars": 10, "80 Stars": 20, "40 Stars": 50,
+            "Perfume Bottle": 10, "Lol Pop": 5, "Trapped Heart": 5
+        }
     },
     "Небо": {
-        "20000 stars": 10, "15000 stars": 15, "10000 stars": 50,
-        "Electric Skull": 7, "Snoop Dogg": 9, "Astral Shard": 1,
-        "Magic Potion": 3, "Input Key": 5
+        "price": 100,
+        "rewards": {
+            "145 Stars": 10, "120 Stars": 20, "80 Stars": 60,
+            "Astral Shard": 5, "Input Key": 5
+        }
     },
     "Сердечный приступ": {
-        "40000 stars": 20, "25000 stars": 40, "Jolly Chimp": 3,
-        "Ice cream": 10, "Snoop Cigar": 5, "Swag Bag": 5,
-        "Winter Wreath": 5, "Clover Pin": 12
+        "price": 130,
+        "rewards": {
+            "170 Stars": 10, "150 Stars": 20, "100 Stars": 50,
+            "Jolly Chimp": 5, "Snoop Cigar": 5, "Trapped Heart": 10
+        }
     },
     "Время": {
-        "50000 stars": 15, "25000 stars": 45, "Kissed Frog": 10,
-        "Snoop Cigar": 5, "Ion Gem": 5, "Astral Shard": 9,
-        "Durov's Cap": 3, "Vintage Cigar": 7, "Swiss Watch": 1
+        "price": 140,
+        "rewards": {
+            "175 Stars": 10, "145 Stars": 20, "120 Stars": 60,
+            "Durov's Cap": 5, "Vintage Cigar": 5
+        }
     },
     "Черный": {
-        "60000 Stars": 20, "30000 Stars": 40, "Perfume Bottle": 10,
-        "Diamond Ring": 5, "Gem signet": 5, "Top Hat": 10,
-        "Toy Bear": 5, "Rare Bird": 5
+        "price": 150,
+        "rewards": {
+            "200 Stars": 10, "150 Stars": 20, "130 Stars": 50,
+            "Gem Signet": 10, "Perfume Bottle": 5, "Snoop Cigar": 5
+        }
     },
     "PEPE": {
-        "50000 Stars": 70, "Map Pumpkin": 20, "Skull Flower": 5,
-        "Sharp Tongue": 4, "Plush Pepe": 1
+        "price": 200,
+        "rewards": {
+            "230 Stars": 10, "170 Stars": 20, "140 Stars": 40,
+            "Plush Pepe": 1, "Skull Flower": 5, "Perfume Bottle": 19, "Vintage Cigar": 5
+        }
     },
     "Мифическая слава": {
-        "70000 Stars": 70, "Loot Bag": 10, "Artisan Brick": 9,
-        "Astral Shard": 5, "Durov's Cap": 1, "Magic Potion": 5
+        "price": 210,
+        "rewards": {
+            "250 Stars": 5, "200 Stars": 10, "180 Stars": 50,
+            "Loot Bag": 15, "Astral Shard": 10, "Durov's Cap": 5, "Magic Potion": 5
+        }
     },
     "alone": {
-        "100000 Stars": 60, "Plush Pepe": 1, "Durov's Cap": 10,
-        "Mighty Arm": 5, "Heroic Halmet": 5, "Perfume Bottle": 19
+        "price": 380,
+        "rewards": {
+            "400 Stars": 10, "340 Stars": 20, "300 Stars": 40,
+            "Plush Pepe": 1, "Durov's Cap": 5, "Perfume Bottle": 19, "Heroic Halmet": 5
+        }
     }
 }
 
+# ========== ЦЕНЫ НА ПРОДАЖУ NFT (ВСЕ ЧТО ЕСТЬ В МАГАЗИНЕ) ==========
+NFT_SELL_PRICES = {
+    "Valentine Box": 30, "Cupid Charm": 30, "Love Potion": 30, "Love Candle": 30, "Cookie Heart": 30,
+    "Trapped Heart": 40, "Snoop Dogg": 50, "Ice Cream": 25, "Happy Brownie": 35, "Lol Pop": 30,
+    "Desk Calendar": 20, "Candy Cane": 25, "Xmas Stocking": 30, "Fresh Socks": 20, "Clover Pin": 25,
+    "Swag Bag": 40, "Snake Box": 45, "Lunar Snake": 50, "B-Day Candle": 20, "Pet Snake": 40,
+    "Pretty Posy": 35, "Snoop Cigar": 55, "Jester Hat": 30, "Stellar Rocket": 50, "Input Key": 45,
+    "Ginger Cookie": 25, "Jolly Chimp": 60, "Jelly Bunny": 40, "Spiced Wine": 35, "Evil Eye": 45,
+    "Spy Agaric": 30, "Winter Wreath": 30, "Star Notepad": 25, "Witch Hat": 35, "Hypno Lollipop": 40,
+    "Santa Hat": 30, "Sakura Flower": 35, "Hanging Star": 30, "Eternal Candle": 40, "Top Hat": 45,
+    "Crystal Ball": 50, "Record Player": 40, "Toy Bear": 35, "Swiss Watch": 60, "Vintage Cigar": 65,
+    "Rare Bird": 70, "Flying Broom": 55, "Low Rider": 60, "Voodoo Doll": 50, "Mad Pumpkin": 45,
+    "Skull Flower": 55, "Astral Shard": 80, "Signet Ring": 50, "Sharp Tongue": 45, "Bonded Ring": 55,
+    "Gem Signet": 70, "Scared Cat": 60, "Electric Skull": 75, "Kissed Frog": 50, "Nail Bracelet": 40,
+    "Loot Bag": 90, "Artisan Brick": 70, "Perfume Bottle": 65, "Durov's Cap": 100, "Ion Gem": 80,
+    "Magic Potion": 85, "Mini Oscar": 90, "Mighty Arm": 95, "Heroic Halmet": 120, "Heart Locket": 80, "Plush Pepe": 150
+}
+
 def get_random_reward(rewards_dict):
+    """Универсальная функция получения случайной награды по шансам"""
     total = sum(rewards_dict.values())
     rand = random.randint(1, total)
     cumulative = 0
